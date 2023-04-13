2c194165b5025b02f302be615bb6d2a920028d0c
MarcosArienti <marcos.arienti311@gmail.com>
2023-04-12 22:34:41 -0300

    Creacion Header y Acerca de mi

--- Proyecto_Integrador_ArientiMarcos/src/app/app.module.ts
+++ Proyecto_Integrador_ArientiMarcos/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule,
