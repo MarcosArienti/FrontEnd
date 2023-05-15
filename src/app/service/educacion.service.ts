816cccebc549bd9e291c043d70de795ed03741eb
MarcosArienti <marcos.arienti311@gmail.com>
2023-05-14 22:32:48 -0300

    bknd test

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,8 +7,8 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/';
-  //URL = 'https://backend-msaa.onrender.com/educacion/'
+  //URL = 'http://localhost:8080/educacion/';
+  URL = 'https://backend-msaa.onrender.com/educacion/'
 
   constructor(private httpClient: HttpClient) { }
 
